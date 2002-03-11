d9cbb16ff347ddd0549b101adeddd43bcec72a97
bradleyhughes <bradleyhughes>
2002-03-11 15:26:29 +0000

    conform to the blackbox code style

--- src/Color.cc
+++ src/Color.cc
@@ -34,19 +34,19 @@ BColor::ColorCache BColor::colorcache;
 bool BColor::cleancache = false;
 
 
-BColor::BColor( int scr )
-    : allocated( false ), r( -1 ), g( -1 ), b( -1 ), p( 0 ), scrn( scr )
+BColor::BColor(int scr)
+    : allocated(false), r(-1), g(-1), b(-1), p(0), scrn(scr)
 {
 }
 
-BColor::BColor( int rr, int gg, int bb, int scr )
-    : allocated( false ), r( rr ), g( gg ), b( bb ), p( 0 ), scrn( scr )
+BColor::BColor(int rr, int gg, int bb, int scr)
+    : allocated(false), r(rr), g(gg), b(bb), p(0), scrn(scr)
 {
 }
 
-BColor::BColor( const string &name, int scr )
-    : allocated( false ), r( -1 ), g( -1 ), b( -1 ), p( 0 ), scrn( scr ),
-      colorname( name )
+BColor::BColor(const string &name, int scr)
+    : allocated(false), r(-1), g(-1), b(-1), p(0), scrn(scr),
+      colorname(name)
 {
     parseColorName();
 }
@@ -56,21 +56,21 @@ BColor::~BColor()
     deallocate();
 }
 
-void BColor::setScreen( int scr )
+void BColor::setScreen(int scr)
 {
-    if ( scr == scrn )
+    if (scr == scrn)
 	return;
 
     deallocate();
 
     scrn = scr;
-    if ( ! colorname.empty() )
+    if (! colorname.empty())
 	parseColorName();
 }
 
 unsigned long BColor::pixel() const
 {
-    if ( ! allocated ) {
+    if (! allocated) {
 	// mutable
 	BColor *that = (BColor *) this;
 	that->allocate();
@@ -81,16 +81,16 @@ unsigned long BColor::pixel() const
 
 void BColor::parseColorName()
 {
-    if ( colorname.empty() ) {
-	fprintf( stderr, "BColor: empty colorname, cannot parse (using black)\n" );
-	setRGB( 0, 0, 0 );
+    if (colorname.empty()) {
+	fprintf(stderr, "BColor: empty colorname, cannot parse (using black)\n");
+	setRGB(0, 0, 0);
     }
 
-    if ( scrn == -1 )
-	scrn = DefaultScreen( BaseDisplay::instance()->x11Display() );
+    if (scrn == -1)
+	scrn = DefaultScreen(BaseDisplay::instance()->x11Display());
 
     BaseDisplay *display = BaseDisplay::instance();
-    Colormap colormap = display->screenInfo( scrn )->colormap();
+    Colormap colormap = display->screenInfo(scrn)->colormap();
 
     // get rgb values from colorname
     XColor xcol;
@@ -102,34 +102,34 @@ void BColor::parseColorName()
     if (! XParseColor(*display, colormap, colorname.c_str(), &xcol)) {
 	fprintf(stderr, "BColor::allocate: color parse error: \"%s\"\n",
 		colorname.c_str());
-	setRGB( 0, 0, 0 );
+	setRGB(0, 0, 0);
 	return;
     }
 
-    setRGB( xcol.red >> 8, xcol.green >> 8, xcol.blue >> 8 );
+    setRGB(xcol.red >> 8, xcol.green >> 8, xcol.blue >> 8);
 }
 
 void BColor::allocate()
 {
-    if ( scrn == -1 ) {
+    if (scrn == -1) {
 	abort();
-	scrn = DefaultScreen( BaseDisplay::instance()->x11Display() );
+	scrn = DefaultScreen(BaseDisplay::instance()->x11Display());
     }
 
     BaseDisplay *display = BaseDisplay::instance();
-    Colormap colormap = display->screenInfo( scrn )->colormap();
+    Colormap colormap = display->screenInfo(scrn)->colormap();
 
-    if ( ! isValid() ) {
-	if ( colorname.empty() ) {
-	    fprintf( stderr, "BColor: cannot allocate invalid color (using black)\n" );
-	    setRGB( 0, 0, 0 );
+    if (! isValid()) {
+	if (colorname.empty()) {
+	    fprintf(stderr, "BColor: cannot allocate invalid color (using black)\n");
+	    setRGB(0, 0, 0);
 	} else
 	    parseColorName();
     }
 
     // see if we have allocated this color before
-    ColorCache::iterator it = colorcache.find( rgb( r, g, b, scrn ) );
-    if ( it != colorcache.end() ) {
+    ColorCache::iterator it = colorcache.find(rgb(r, g, b, scrn));
+    if (it != colorcache.end()) {
 	// found
 	allocated = true;
 	p = (*it).second.p;
@@ -146,43 +146,43 @@ void BColor::allocate()
 
     if (! XAllocColor(*display, colormap, &xcol)) {
 	fprintf(stderr, "BColor::allocate: color alloc error: rgb:%x/%x/%x\n",
-		r, g, b );
+		r, g, b);
 	xcol.pixel = 0;
     }
 
     p = xcol.pixel;
     allocated = true;
 
-    std::pair<rgb,pixelref> pr( rgb( r, g, b, scrn ), pixelref( p ) );
-    colorcache.insert( pr );
+    std::pair<rgb,pixelref> pr(rgb(r, g, b, scrn), pixelref(p));
+    colorcache.insert(pr);
 
-    if ( cleancache )
+    if (cleancache)
 	doCacheCleanup();
 }
 
 void BColor::deallocate()
 {
-    if ( ! allocated )
+    if (! allocated)
 	return;
 
-    ColorCache::iterator it = colorcache.find( rgb( r, g, b, scrn ) );
-    if ( it != colorcache.end() )
-	if ( (*it).second.count < 1 )
+    ColorCache::iterator it = colorcache.find(rgb(r, g, b, scrn));
+    if (it != colorcache.end())
+	if ((*it).second.count < 1)
 	    (*it).second.count = 0;
 	else
 	    (*it).second.count--;
 
-    if ( cleancache )
+    if (cleancache)
 	doCacheCleanup();
 
     allocated = false;
 }
 
-BColor &BColor::operator=( const BColor &c )
+BColor &BColor::operator=(const BColor &c)
 {
     deallocate();
 
-    setRGB( c.r, c.g, c.b );
+    setRGB(c.r, c.g, c.b);
     colorname = c.colorname;
     scrn = c.scrn;
     return *this;
@@ -200,11 +200,11 @@ void BColor::doCacheCleanup()
     unsigned long *pixels = new unsigned long[ colorcache.size() ];
     int i, count;
 
-    for ( i = 0; i < display->screenCount(); i++ ) {
+    for (i = 0; i < display->screenCount(); i++) {
 	count = 0;
 	ColorCache::iterator it = colorcache.begin();
-	while ( it != colorcache.end() ) {
-	    if ( (*it).second.count != 0 || (*it).first.screen != i ) {
+	while (it != colorcache.end()) {
+	    if ((*it).second.count != 0 || (*it).first.screen != i) {
 		it++;
 		continue;
 	    }
@@ -212,12 +212,12 @@ void BColor::doCacheCleanup()
 	    pixels[ count++ ] = (*it).second.p;
 	    ColorCache::iterator it2 = it;
 	    it++;
-	    colorcache.erase( it2 );
+	    colorcache.erase(it2);
 	}
 
-	if ( count > 0 )
-	    XFreeColors( *display, display->screenInfo( i )->colormap(),
-			 pixels, count, 0 );
+	if (count > 0)
+	    XFreeColors(*display, display->screenInfo(i)->colormap(),
+			 pixels, count, 0);
     }
 
     delete [] pixels;
